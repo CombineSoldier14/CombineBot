c6804ddaeb073daec97ac2a018d93d01b65660c2
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-15 00:53:43 -0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,62 @@
 const { Events } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { error_reports } = require('./webhooks.js')
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
 		if (!interaction.isChatInputCommand()) return;
+		const errorEmbed = new EmbedBuilder()
+			.setColor(0x00FF0000)
+			.setTitle('Whoops!')
+			.setDescription('There was an error executing this command. Try again, or this could be an internal code error.')
+			.setThumbnail('https://i.imgur.com/KR3aiwB.png');
+
+		const reportErrorButton = new ButtonBuilder()
+			.setCustomId('errorReporter')
+			.setLabel('Report Error to CombineSoldier14')
+			.setStyle(ButtonStyle.Primary)
+
+		const reportGithubIssue = new ButtonBuilder()
+			.setLabel('Open Github Issue')
+			.setStyle(ButtonStyle.Link)
+			.setURL('https://github.com/CombineSoldier14/CombineJS/issues/new')
+
+
+		const errorRow = new ActionRowBuilder()
+			.addComponents(reportErrorButton, 
+				reportGithubIssue
+			)
+
+		const response = await interaction.reply({ embeds: [errorEmbed], components: [errorRow] });
+
+		const collectorFilter = i => i.user.id === interaction.user.id;
+
+		const confirmation = await response.awaitMessageComponent({ filter: collectorFilter });
+
+		if (confirmation.customId === 'errorReporter') {
+			errorRow.components[0].setDisabled(true)
+			errorRow.components[0].setLabel('Error Reported!')
+			await interaction.editReply({ components: [errorRow] })
+			await interaction.followUp({ content: 'Error has been reported!' });
+			fetch(error_reports, {
+				method: "POST",
+				body: JSON.stringify({
+				  content: `<@951639877768863754>\n# Error Occurred!\n Command: /${interaction.commandName}`
+				}),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+					"User-Agent": "CombineJS (https://github.com/CombineSoldier14/CombineJS +combineemails14@gmail.com); curl/8.4.0",
+					"Accept": "application/json,text/plain,application/xml",
+					"Upgrade-Insecure-Requests": "1",
+					"Accept-Encoding": "gzip",
+					"Connection": "close"
+				}
+			  })
+			    .then((response) => response.json())
+  			    .then((json) => console.log(json));
+		}
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
@@ -17,9 +70,7 @@ module.exports = {
 		} catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				response
 			}
 		}
 	},
