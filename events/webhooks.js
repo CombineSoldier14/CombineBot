c0a43b688de582841eaf7b41d26546e5201e82b7
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-15 00:54:10 -0700

    Add files via upload

---
+++ events/webhooks.js
@@ -0,0 +1,5 @@
+const webhooks = [
+    {
+        "error_reports":"https://discord.com/api/webhooks/1259298998301495379/R6zd6M4D2SQ_l2DfL-3vaEEBNBtU4XuZODbrWHnq0IR0Xj4IZcgvpvS2XrHMpr1YqHXD"
+    }
+]
