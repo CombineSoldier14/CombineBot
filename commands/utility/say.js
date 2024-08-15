f2821a90fb4ac29db10427ac395a9a111b3e59ff
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-14 17:20:27 -0700

    Add files via upload

---
+++ commands/utility/say.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('say')
+        .setDescription('Make the bot say a message!')
+        .addStringOption(option =>
+            option
+                .setName('text')
+                .setDescription('Text for the bot to say!')
+                .setRequired(true)
+        ),
+        async execute(interaction) {
+            const text = interaction.options.getString('text')
+            await interaction.reply({ content: "Message has been sent!", ephemeral: true })
+            await interaction.channel.send(text)
+        }
+}
