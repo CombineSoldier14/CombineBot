de94f6a13462cd365e4d9f77b8080214fd0c8e84
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-14 17:19:18 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,30 @@
 const { Events } = require('discord.js');
+const { statuses } = require('./statuses.js');
+const { ActivityType } = require('discord.js');;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		for (let status in statuses) {
+			if (statuses[status].type == "PLAYING") {
+				var truetype = ActivityType.Playing;
+			} else {
+				var truetype = ActivityType.Watching;
+			};
+			client.user.setPresence({ 
+				activities: [{ 
+					name: statuses[status].status, 
+					type: truetype
+				}],
+				status: 'online' 
+			});
+			await sleep(30000);
+		};
 	},
 };
