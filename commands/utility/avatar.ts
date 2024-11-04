b5ef9b9dda61e61d75248d20922f94499bb0b59e
combinesoldier14 <littlelatta@outlook.com>
2024-11-03 20:29:16 -0800

    added avatar command and fixed some bugs in core exports

---
+++ commands/utility/avatar.ts
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { embed } = require('../../core/combinejs.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Get the avatar of a user!')
+        .addUserOption((option: any) =>
+            option
+                .setName('user')
+                .setDescription('User to get avatar of')
+                .setRequired(true)
+
+        ),
+    async execute (interaction: any) {
+        const user = interaction.options.getUser('user');
+        embed.setTitle(`${user.username}'s avatar`);
+        embed.setImage(`https://cdn.discordapp.com/avatars/${String(user.id)}/${String(user.avatar)}.png?size=1024`);
+        await interaction.reply({ embeds: [embed] });
+    },
+};
+
+export {};
