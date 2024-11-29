4ef65db486f6b55e8e47db43328f50ed7bc11c59
Holden Latta <101304773+CombineSoldier14@users.noreply.github.com>
2024-11-29 12:08:17 -0800

    Add files via upload

---
+++ commands/apis/pokedex.ts
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { CombineJS } = require('../../core/combinejs.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pokedex')
+        .setDescription('Search the Pokedex for info on a Pokemon!')
+        .addStringOption((option: any) =>
+            option
+                .setName('pokemon')
+                .setDescription('Pokemon to get info of')
+                .setRequired(true)
+        ),
+    async execute(interaction: any) {
+        await interaction.deferReply();
+        const pokemon = interaction.options.getString('pokemon');
+        const r = await CombineJS.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        const status = r.status;
+        if (status == 404) {
+            await interaction.editReply(":x: Pokemon not found! Did you misspell it?");
+            return;
+        }
+        const j = await r.json();
+        let abilities = "";
+        for (let ability of j.abilities) {
+            abilities = abilities + `${ability.ability.name}`;
+        }
+        let embed = await CombineJS.embed();
+        embed
+            .setTitle(`Info on ${pokemon}`)
+            .setDescription(`
+**Abilities:** ${abilities}\n
+**Base XP:** ${j.base_experience}\n
+**Order:** ${j.order}\n
+**Weight:** ${j.weight}\n
+**Height:** ${j.height}\n
+`)
+            .setThumbnail(j.sprites.front_default);
+        await interaction.editReply({ embeds: [embed] });
+    }
+}
+
+export {};
