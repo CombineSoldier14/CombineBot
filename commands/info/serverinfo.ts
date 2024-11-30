0a356c849437a282d0ff1020e18013c5de8d9ce1
Holden Latta <101304773+CombineSoldier14@users.noreply.github.com>
2024-11-30 12:47:48 -0800

    Update serverinfo.ts

--- commands/info/serverinfo.ts
+++ commands/info/serverinfo.ts
@@ -7,7 +7,7 @@ module.exports = {
         .setName('serverinfo')
         .setDescription('Shows detailed info on the server you\'re in'),
     async execute (interaction: any) {
-            let embed = CombineJS.embed();
+            let embed = await CombineJS.embed();
             embed.setTitle(`Info on ${interaction.guild.name}`);
             embed.setDescription(`Created at: ${interaction.guild.createdAt}\nID: ${interaction.guild.id}\nMembers: ${interaction.guild.memberCount}`);
             embed.setThumbnail(`https://cdn.discordapp.com/icons/${interaction.guild.id}/${interaction.guild.icon}.png?size=1024`);
