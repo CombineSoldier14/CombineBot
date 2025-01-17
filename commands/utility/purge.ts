ad72b4f894c8018bb6e1f4df915b370571c874ac
CombineSoldier14 <101304773+CombineSoldier14@users.noreply.github.com>
2025-01-16 21:57:32 -0800

    Create purge.ts

---
+++ commands/utility/purge.ts
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Delete a certain amount of recent messages.')
+        .addNumberOption((option: any) =>
+            option
+                .setName('amount')
+                .setDescription('Amount of messages to delete. Must be lower than 100!')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const amount = interaction.options.getNumber('amount');
+      
+        if (amount < 1 || amount > 100) {
+            return await interaction.reply({
+                content: 'Please provide a number between 1 and 100.',
+                ephemeral: true
+            });
+        }
+      
+        try {
+            await interaction.channel.bulkDelete(amount, true); // 'true' filters out messages older than 14 days
+            await interaction.reply({
+                content: `:white_check_mark: Successfully deleted **${amount}** messages.`,
+                ephemeral: true
+            });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({
+                content: `:x: An error occurred! Did you specify more than 100 messages?`,
+                ephemeral: true
+            });
+        }
+    }
+};
